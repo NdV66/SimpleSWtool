ed759fa101926b2286a211d5af5ad3a4d695b51a
Marta Zażlak <marta.zazlak@hotmail.com>
2017-03-27 21:03:52 +0200

    Adding features and bugfixing

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 'use strict';
 /**
- * Thank you, https://swapi.co/ :)
+ * Thank you, https://swapi.co/
+ * And http://starwars.wikia.com/wiki/  :)
  */
-
 const fs = require('fs');
 const tools = require('./lib/other-tools');
 const swapiTools = require('./lib/swapi-tools');
-const wikiTools = require('./lib/wookpedia-tools');
+const wikiTools = require('./lib/wookieepedia-tools');
 
 let swapiPlanetUrl = 'http://swapi.co/api/planets/';
+const pathToSave = './planets.json';
 
-//tools.makeRequest(swapiPlanetUrl)
-//    .then(
-//        (result) => {
-//            return swapiTools.createJSONfromElements(swapiPlanetUrl, 1, parseInt(JSON.parse(result).count));
-//        },
-//        (error) => {
-//            throw new Error(error);
-//        })
-//    .then(
-//        (planets) => {
-//            console.log(planets);
-//            fs.writeFile('./planets.json', JSON.stringify(planets));
-//        },
-//        (error) => {
-//            throw new Error(error);
-//        })
-//    .catch((error) => {
-//        console.log(`So sad... ${error}`);
-//    });
-wikiTools.parseElement('Tatooine');
+tools.makeRequest(swapiPlanetUrl)
+    .then(
+        (result) => {
+            return swapiTools.getAllPlanetsNames(swapiPlanetUrl, parseInt(JSON.parse(result).count)); //parseInt(JSON.parse(result).count)
+        },
+        (error) => {
+            throw new Error(error);
+        })
+    .then(
+        (planets) => {
+            return wikiTools.parseElementsToOneJSON(planets);
+        },
+        (error) => {
+            throw new Error(error);
+        })
+    .then(
+        (result) => {
+            fs.writeFile(pathToSave, JSON.stringify(result));
+            console.log('Check your file :)');
+        },
+        (error) => {
+            throw new Error(error);
+        })
+    .catch((error) => {
+        console.log(`So sad... ${error}`);
+    });
 
 
