da6d3462e78f0125ce86dad6db9eb44d71b0ddbb
Marta Zażlak <marta.zazlak@hotmail.com>
2017-03-27 16:01:46 +0200

    Add tool for sw wiki

--- main.js
+++ main.js
@@ -4,26 +4,31 @@
  */
 
 const fs = require('fs');
-const tools = require('./lib/tools');
-let startUrl = 'http://swapi.co/api/planets/';
+const tools = require('./lib/other-tools');
+const swapiTools = require('./lib/swapi-tools');
+const wikiTools = require('./lib/wookpedia-tools');
 
-tools.makeRequest(startUrl)
-    .then(
-        (result) => {
-            return tools.createJSONfromElements(startUrl, 1, parseInt(JSON.parse(result).count));
-        },
-        (error) => {
-            throw new Error(error);
-        })
-    .then(
-        (planets) => {
-            console.log(planets);
-            fs.writeFile('./planets.json', JSON.stringify(planets));
-        },
-        (error) => {
-            throw new Error(error);
-        })
-    .catch((error) => {
-        console.log(`So sad... ${error}`);
-    });
+let swapiPlanetUrl = 'http://swapi.co/api/planets/';
+
+//tools.makeRequest(swapiPlanetUrl)
+//    .then(
+//        (result) => {
+//            return swapiTools.createJSONfromElements(swapiPlanetUrl, 1, parseInt(JSON.parse(result).count));
+//        },
+//        (error) => {
+//            throw new Error(error);
+//        })
+//    .then(
+//        (planets) => {
+//            console.log(planets);
+//            fs.writeFile('./planets.json', JSON.stringify(planets));
+//        },
+//        (error) => {
+//            throw new Error(error);
+//        })
+//    .catch((error) => {
+//        console.log(`So sad... ${error}`);
+//    });
+wikiTools.parseElement('Tatooine');
+
 
